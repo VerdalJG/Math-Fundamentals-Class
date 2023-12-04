6c7ca18938a90783ec73ef259a2ee009d1700bbe
VerdalJG <88113293+VerdalJG@users.noreply.github.com>
2023-12-04 21:22:56 +0100

    Prac 2 - Transformation matrices fixed, mesh renders

--- exercise2handout/node.cpp
+++ exercise2handout/node.cpp
@@ -33,11 +33,39 @@ void  Node::updateLocal()
 { 
 	// todo: given position, rotation and scale, create T,R and S such that
 
-	//localMatrix = T*R*S;
+	mat4 T = identity_mat4();
+	T.col[3].x = position.x;
+	T.col[3].y = position.y;
+	T.col[3].z = position.z;
+	
+	mat4 R = quat_to_mat4(rotation);
+
+	mat4 S = identity_mat4();
+	S.c[0][0] = scale.x;
+	S.c[1][1] = scale.y;
+	S.c[2][2] = scale.z;
+	
+	localMatrix = T*R*S;
 
 	// todo: given all above, create Sinv, Rinv and Tinv such that
 
-	//localInverseMatrix = Sinv*Rinv*Tinv;
+	mat4 TInverse = identity_mat4();
+	T.col[3].x = -position.x;
+	T.col[3].y = -position.y;
+	T.col[3].z = -position.z;
+
+	versor rotationInverted = rotation;
+	rotationInverted.w *= -1;
+
+	mat4 RInverse = quat_to_mat4(rotationInverted);
+
+
+	mat4 SInverse = identity_mat4();
+	S.c[0][0] = 1 / scale.x;
+	S.c[1][1] = 1 / scale.y;
+	S.c[2][2] = 1 / scale.z;
+
+	localInverseMatrix = SInverse * RInverse * TInverse;
 }
 
 void  Node::updateHierarchy()
